5516a5ef0ce6403431e17769c023b67c4a3a0f90
arpitkarnatak <arpitkarnatak47@gmail.com>
2023-02-17 01:49:19 +0530

    Removed * from cors

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const PORT = AppEnv.PORT || 3000
 app.use(logger)
 app.use(bodyParser.json())
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', AppEnv.FRONTEND_URL);
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
